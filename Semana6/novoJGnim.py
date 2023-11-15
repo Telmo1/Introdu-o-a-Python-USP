6eab8a9a439f439c4af50393e2ba3f076f91147b
Telmo <yrtelmo00@gmail.com>
2023-11-14 21:04:20 -0300

    Semana07

--- Semana6/novoJGnim.py
+++ Semana6/novoJGnim.py
@@ -1,16 +1,20 @@
 def usuario_escolhe_jogada(n, m):
     jogada = 0
     while jogada <= 0 or jogada > m or jogada > n:
-        jogada = int(input(f"Quantas peças você quer tirar (máximo {min(m, n)} peças)? "))
+        jogada = int(input(f"Quantas peças você quer tirar (máximo {min(m, n)})? "))
         if jogada <= 0 or jogada > m or jogada > n:
             print(f"Jogada inválida. Tente novamente.")
     return jogada
 
 def computador_escolhe_jogada(n, m):
-    if n % (m+1) == 0:
-        return m
+    if n <= m:
+        return n
     else:
-        return n % (m+1)
+        resto = n % (m + 1)
+        if resto > 0:
+            return resto
+        else:
+            return m
 
 def partida():
     n = int(input("Digite o número de peças: "))
@@ -33,16 +37,41 @@ def partida():
 
     if computador_comeca_jogo:
         print("Você ganhou!")
+        return "usuario"
     else:
         print("Fim do jogo! O computador ganhou!")
+        return "computador"
 
 def computador_comeca(n, m):
-    if (n % (m+1)) == 0:
+    if (n % (m + 1)) == 0:
         print("Você começa!")
         return False
     else:
         print("Computador começa!")
         return True
 
-    
-partida()
+def campeonato():
+    placar_usuario = 0
+    placar_computador = 0
+
+    for _ in range(3):  # Realiza três partidas
+        resultado_partida = partida()
+
+        if resultado_partida == "usuario":
+            placar_usuario += 1
+        else:
+            placar_computador += 1
+
+    print("\nPlacar final do campeonato:")
+    print(f"Você {placar_usuario} X {placar_computador} Computador")
+
+    if placar_usuario > placar_computador:
+        print("Você venceu o campeonato!")
+    elif placar_usuario < placar_computador:
+        print("O computador venceu o campeonato!")
+    else:
+        print("O campeonato terminou em empate!")
+
+# Ao final do seu código, chame a função campeonato():
+campeonato()
+
---
+++ Semana7/largura.py
@@ -0,0 +1,7 @@
+largura = int(input("Digite a largura do retângulo: "))
+altura = int(input("Digite a altura do retângulo: "))
+
+linha = "#" * largura
+
+for i in range(altura):
+    print(linha)
